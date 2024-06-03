cd7b8fcb9dcfb4a024401a40030f07ed4ffc332d
Ankita Sharma <149663326+ankimsharma@users.noreply.github.com>
2024-06-03 20:41:29 +0530

    Update main.tf

--- terraform_start/main.tf
+++ terraform_start/main.tf
@@ -14,13 +14,13 @@ provider "azurerm" {
 }
 
 # Create a resource group
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "testTF_resource_group" {
   name     = "example-resources"
   location = "West Europe"
 }
 
 # Create a virtual network within the resource group
-resource "azurerm_virtual_network" "example" {
+resource "azurerm_virtual_network" "TestTF_vitual_network" {
   name                = "example-network"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
